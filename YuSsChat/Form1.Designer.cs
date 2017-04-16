29114be1a6d214bf5438b2b1326ab3c4dde94116
Yuriy Sokolsky <YuSokolsky@gmail.com>
2017-04-16 15:05:26 +0300

    first commit

---
+++ YuSsChat/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace YuSsChat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.SendMbtn = new System.Windows.Forms.Button();
+            this.MainChat = new System.Windows.Forms.RichTextBox();
+            this.ChatTextBox = new System.Windows.Forms.TextBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnDisconnect = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // SendMbtn
+            // 
+            this.SendMbtn.Location = new System.Drawing.Point(140, 325);
+            this.SendMbtn.Name = "SendMbtn";
+            this.SendMbtn.Size = new System.Drawing.Size(121, 40);
+            this.SendMbtn.TabIndex = 0;
+            this.SendMbtn.Text = "Отправить";
+            this.SendMbtn.UseVisualStyleBackColor = true;
+            this.SendMbtn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // MainChat
+            // 
+            this.MainChat.Location = new System.Drawing.Point(16, 14);
+            this.MainChat.Name = "MainChat";
+            this.MainChat.ReadOnly = true;
+            this.MainChat.Size = new System.Drawing.Size(741, 279);
+            this.MainChat.TabIndex = 1;
+            this.MainChat.Text = "";
+            this.MainChat.TextChanged += new System.EventHandler(this.richTextBox_TextChanged);
+            // 
+            // ChatTextBox
+            // 
+            this.ChatTextBox.Location = new System.Drawing.Point(16, 299);
+            this.ChatTextBox.Name = "ChatTextBox";
+            this.ChatTextBox.Size = new System.Drawing.Size(245, 20);
+            this.ChatTextBox.TabIndex = 2;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(16, 371);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(121, 35);
+            this.btnConnect.TabIndex = 3;
+            this.btnConnect.Text = "Подключиться";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnDisconnect
+            // 
+            this.btnDisconnect.Location = new System.Drawing.Point(140, 371);
+            this.btnDisconnect.Name = "btnDisconnect";
+            this.btnDisconnect.Size = new System.Drawing.Size(121, 35);
+            this.btnDisconnect.TabIndex = 4;
+            this.btnDisconnect.Text = "Отсоедениться";
+            this.btnDisconnect.UseVisualStyleBackColor = true;
+            this.btnDisconnect.Click += new System.EventHandler(this.btnDisconnect_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(34, 410);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(140, 413);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "<-=Канал";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(297, 325);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 40);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Обнулить голоса за дроп";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(297, 372);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(539, 339);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "label3";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 900000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(803, 503);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnDisconnect);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.ChatTextBox);
+            this.Controls.Add(this.MainChat);
+            this.Controls.Add(this.SendMbtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button SendMbtn;
+        private System.Windows.Forms.RichTextBox MainChat;
+        private System.Windows.Forms.TextBox ChatTextBox;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnDisconnect;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ YuSsChat/TwitchTV.cs
@@ -0,0 +1,19 @@
+﻿namespace TwitchAPIViewer
+{
+    public class TwitchTV
+    {
+        public Stream stream { get; set; }
+    }
+
+    public class Stream
+    {
+        public long viewers { get; set; }
+        public Channel channel { get; set; }
+    }
+
+    public class Channel
+    {
+        public long followers { get; set; }
+        public long views { get; set; }
+    }
+}
